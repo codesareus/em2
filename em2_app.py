5ad405c5af744718d83bba2e1a4452f82de8ad00
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-09 18:19:58 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -111,7 +111,10 @@ runa, runb = (runner_x - 300), ( runner_y - 200) # Example position
 for offset in [(0, 0), (1, 0), (0, 1), (1, 1)]:  
     draw.text(( runa- 10 + offset[0], runb - 10 + offset[1]), 
               moon_text, fill="navy", font=font)
-    
+
+for offset in [(0, 0), (2, 0), (0, 2), (2, 2)]:  
+    draw.text(( runa- 10 + offset[0], runb - 10 + offset[1]), 
+              moon_text, fill="navy", font=font)
 # Add a small arrow pointing down from the label
 arrow_start = (runner_x, runner_y - 20)  # Start of the arrow (just below the label)
 arrow_end = (runner_x, runner_y-10)        # End of the arrow (pointing to the runner)
