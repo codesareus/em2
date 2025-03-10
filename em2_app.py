b582b6b42f43a079544c9de52c6129ef3dc9f285
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-09 19:31:37 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -57,7 +57,7 @@ draw.ellipse(
         (MOON_POSITION[0] + MOON_RADIUS, MOON_POSITION[1] + MOON_RADIUS),
     ],
     #fill="#ADD8E6",
-    fill="navy",
+    fill="skyblue",
     outline="yellow",
 )
 
