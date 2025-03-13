42143a77305b66f50226d61ae8652e5b5573bd47
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-13 08:30:54 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -726,15 +726,18 @@ def single_correlation(dataA, dataB, num=0):
     axes[num].legend()
 
 #############
+def onePlot_corr(num = 0):
+    fig, axes = plt.subplots(2, 1, figsize=(12,9), gridspec_kw={'height_ratios': [50, 1 ]})
+#fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 8))  # 8 rows, 1 column
+    dataaa, databb = data_pairs[num]
+    single_correlation(dataaa, databb, 0)
+    st.pyplot(fig)
+
 #############-$# Plot correlation between smoothed data1 and data2
 data_list = [data, data1, data2, data3, data4]
 data_pairs = [(data, data1),(data, data2),(data1, data2),(data4, data3),(data, data3),(data, data4),(data1, data3),(data2, data3)]
 
-fig, axes = plt.subplots(2, 1, figsize=(12,9), gridspec_kw={'height_ratios': [9,1 ]})
-#fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 8))  # 8 rows, 1 column
-dataaa, databb = data_pairs[3]
-single_correlation(dataaa, databb, 0)
-st.pyplot(fig)
+onePlot_corr(3)
 
 fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10, 12))  # 8 rows, 1 column
 for i, (dataA, dataB) in enumerate(data_pairs[:3]):
