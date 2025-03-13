94e88f7f562adb8222faf189caf978ce37f16efe
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-13 13:17:13 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -607,7 +607,7 @@ def single_correlation(dataA, dataB, num=0):
 
     last_21_days_data2 = dataB[-30:] ### borrowed variable name
     #last_21_days_time_steps = np.arange(len(user_data1_smooth) -60, len(user_data1_smooth)).reshape(-1, 1)
-    axes[num].scatter(last_21_days_time_steps, last_21_days_data2, color="gray", marker=".", s=60)
+    axes[num].scatter(last_21_days_time_steps, last_21_days_data2, color="darkgray", marker=".", s=60)
     
     # Add trend lines and calculate R-squared values
     X = np.arange(len(user_data1_smooth)).reshape(-1, 1)
