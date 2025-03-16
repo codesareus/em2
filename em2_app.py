51a9a9558e1ce78a35629a791dbedc0493dc7507
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-16 06:58:05 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -400,7 +400,7 @@ if data is not None and data1 is not None and data2 is not None and data3 is not
 # Position and content for the text annotation
     current_date = datetime.now(LOCAL_TIMEZONE).strftime("%Y年%m月%d日")
     wrapped_input = wrap_text(marker_message_input, 12)  # Wrap text to 25 characters per line
-    dayData = f"慢跑第{days_difference}天, {current_date}: \n耳鸣：{data[-1]}, 脾胃：{data1[-1]}, 睡眠：{data2[-1]},慢跑心率：{data3[-1] *16}, 时长：{data4[-1]},"
+    dayData = f"慢跑第{days_difference}天, {current_date}: \n耳鸣：{data[-1]}, 脾胃：{data1[-1]}, 睡眠：{data2[-1]},慢跑心率：{data3[-1] *16}, 时长：{data4[-1]*10},"
     
     ax.text(v_line_pos, 3.5,
         set_message +  "\n"+ dayData +"\n"+ wrapped_input,
