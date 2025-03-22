45e17f13e36f016c7e1fc51c8e2e18ee07678ca1
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-21 19:07:21 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -754,12 +754,9 @@ def single_correlation(dataA, dataB, num=0):
     
    # st.markdown(f"Date: {current_date}")
     # Display correlation coefficient below the plot
-    title = f"{current_date}{nameA}和{nameB}双动态均值：相关性和趋势分析" 
+    title = f"{current_date}慢跑第{days_difference}天,{nameA}和{nameB}双动态均值：相关性和趋势分析" 
     correlation_coeff = f"__相关系数:{np.corrcoef(user_data1_smooth, user_data2_smooth)[0, 1]:.2f}"
     #print(22, f"**Correlation Coefficient:** {np.corrcoef(user_data1_smooth, user_data2_smooth)[0, 1]:.2f}")
-    axes[num].text(0.3, 0.9, f"慢跑第{days_difference}天\n{today_date}", 
-        horizontalalignment='right', verticalalignment='top', 
-        fontsize=12, color="blue")
     axes[num].set_xlabel("天数")
     axes[num].set_ylabel("双动态均值")
     axes[num].set_title(title + correlation_coeff)
