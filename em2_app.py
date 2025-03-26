fb3649cabb3b5438199d155c6a3a1b6b5df84273
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-26 07:16:14 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -709,7 +709,7 @@ def single_correlation(dataA, dataB, num=0):
     
     # Plot trend lines
     axes[num].plot(X, y_pred_linear_data1, color='blue', linestyle='--', label=f"线性趋势 ({nameA}, $R^2$={r2_linear_data1:.2f})")
-    axes[num].plot(X, y_pred_poly_data1, color='blue', linestyle=':', label=f"多项式趋势 ({nameA},{prdegree}次 $R^2$={r2_poly_data1:.2f})")
+    axes[num].plot(X, y_pred_poly_data1, color='blue', linestyle=':', label=f"多项式趋势 ({nameA},{st.session_state.prdegree}次 $R^2$={r2_poly_data1:.2f})")
     
     axes[num].plot(X, y_pred_linear_data2, color='orange', linestyle='--', label=f"线性趋势 ({nameB}, $R^2$={r2_linear_data2:.2f})")
     axes[num].plot(X, y_pred_poly_data2, color='orange', linestyle=':', label=f"多项式趋势 ({nameB},{prdegree}次 $R^2$={r2_poly_data2:.2f})")
