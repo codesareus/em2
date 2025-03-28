cab7c935c61ec154aeed46351223f7064545c7d8
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-28 10:05:22 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -282,13 +282,13 @@ with st.sidebar:
             #dataSet = [series.strip('"') for series in series_list]
 
             # Process dataSet3 (index 2) by multiplying each data point with 16
-            result[3] = [ x * 16 for x in result[3]]
-            result[4] = [ x * 10 for x in result[4]]
+            #result[3] = [ x * 16 for x in result[3]]
+            #result[4] = [ x * 10 for x in result[4]]
     
             st.write(len(result))
             st.write(result[4])
             save_data(result[0], result[1], result[2], result[3], result[4])
-            st.rerun()
+            
             
 # Process dataSet4 (index 3) by multiplying each data point with 10
             #original_list2 = parse_input(dataSet[4])  
