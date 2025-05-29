096fceb82e9e8b1f38cdef320b73d42a73ee4254
codesareus <125232658+codesareus@users.noreply.github.com>
2025-05-29 06:20:54 -0500

    Update em2_app.py

--- em2_app.py
+++ em2_app.py
@@ -908,8 +908,8 @@ data3= [round(x /1.6, 1) for x in dataa]
 datab = parse_input(wqshijian_input)
 data4 = [round(x /1.0, 1) for x in datab] 
 
-dataSets =[data, data2, data3, data4]
-names = ['耳鸣级数','睡眠质量', '5K心率均值（最高值百分比）','5K时长(分钟）']
+dataSets =[data, data4，data3，data2]
+names = ['耳鸣级数','5K时长(分钟）'，'5K心率均值（最高值百分比）'，'睡眠质量']
 
 # Create a single figure with 4 subplots stacked vertically
 fig, axes = plt.subplots(nrows=4, ncols=1, figsize=(10, 2.5*4))
